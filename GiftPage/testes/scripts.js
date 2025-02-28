1907fa76f36947871bd6b54514d5879cd615a492
Fabian <160931865+FabianMessy@users.noreply.github.com>
2025-02-27 22:21:41 -0300

    Add files via upload
    aaa

--- GiftPage/testes/scripts.js
+++ GiftPage/testes/scripts.js
@@ -1,22 +1,26 @@
 document.addEventListener('scroll', function() {
-    var scrollPosition = window.pageYOffset;
+    var posScroll = window.pageYOffset;
     var maxScroll = document.documentElement.scrollHeight - window.innerHeight;
 
-    // Definir os pontos de início e fim da animação como percentuais da altura total da página
-    var startScroll = maxScroll * 0.50; // 20% do scroll total
-    var endScroll = maxScroll * 0.60; // 30% do scroll total
+    var escalaVelocidade = 10; // maior o valor, mais lento o texto se move
 
-    if (scrollPosition > startScroll && scrollPosition < endScroll) {
-        // Calcula a posição do texto entre 0px e 100% da largura da janela (ou mais, se necessário)
-        var textPosition = ((scrollPosition - startScroll) / (endScroll - startScroll)) * window.innerWidth;
-        document.getElementById('animated-text').style.left = textPosition + 'px';
-    } else if (scrollPosition <= startScroll) {
-        // Mantém o texto à esquerda antes de 20% de rolagem
-        document.getElementById('animated-text').style.left = '0px';
-    } else {
-        // Mantém o texto à direita depois de 30% de rolagem
-        document.getElementById('animated-text').style.left = window.innerWidth + 'px';
-    }
+    var posText = (posScroll / maxScroll) * (escalaVelocidade + window.innerWidth); // calc nova pos
+    document.getElementById('animated-text').style.left = posText + 'px';
+
+    posText = Math.min(window.innerWidth - document.getElementById('animated-text').offsetWidth, posText);
 });
 
+document.addEventListener('scroll', function() {
+    var posScroll = window.pageYOffset;
+    var maxScroll = document.documentElement.scrollHeight - window.innerHeight;
 
+    var escalaVelocidade = 150; // Quanto maior o valor, mais lento o texto se move
+
+    // Calcula a nova posição começando da direita para a esquerda
+    var posText = window.innerWidth - ((posScroll / maxScroll) * (escalaVelocidade + window.innerWidth));
+
+    // Garante que o texto não saia da tela à direita
+    posText = Math.min(window.innerWidth - document.getElementById('animated-text2').offsetWidth, posText);
+
+    document.getElementById('animated-text2').style.left = posText + 'px';
+});
